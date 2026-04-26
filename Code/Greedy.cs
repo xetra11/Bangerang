2af45f9b1310a4a06133aecdfbfa25ad66d3477a
xetra11 <xetra11@outlook.com>
2026-04-26 04:54:39 +0200

    feat(player): re-add player prefab and coin sound logic

--- Code/Greedy.cs
+++ Code/Greedy.cs
@@ -3,17 +3,18 @@ using Sandbox.Diagnostics;
 
 public sealed class Greedy : Component, Component.ITriggerListener
 {
-	[Property] public int GreedValue { get; set; }
+  [Property] public int GreedValue { get; set; }
+  [Property] public SoundEvent CoinSound { get; set; }
 
-	protected override void OnUpdate()
-	{
-	}
+  protected override void OnUpdate()
+  {
+  }
 
-	public void OnTriggerEnter( Collider other )
-	{
-		Component player = other.GameObject.GetComponentInParent<Player>();
-		if (!player.IsValid) return;
-		DestroyGameObject();
-	}
-
+  public void OnTriggerEnter( Collider other )
+  {
+    Component player = other.GameObject.GetComponentInParent<Player>();
+    if ( !player.IsValid ) return;
+    if ( CoinSound != null ) Sound.Play( CoinSound, WorldPosition );
+    DestroyGameObject();
+  }
 }
--- Code/PlayerGadgetInput.cs
+++ Code/PlayerGadgetInput.cs
@@ -4,13 +4,13 @@ namespace Sandbox;
 
 public sealed class PlayerGadgetInput : Component
 {
-	[Property] public SelectedGadget SelectedGadget { get; set; }
+  [Property] public SelectedGadget SelectedGadget { get; set; }
 
-	protected override void OnUpdate()
-	{
-		if ( Input.Released( "attack1" ) )
-		{
-			SelectedGadget.Fire();
-		}
-	}
+  protected override void OnUpdate()
+  {
+    if ( Input.Released( "attack1" ) )
+    {
+      SelectedGadget.Fire();
+    }
+  }
 }
