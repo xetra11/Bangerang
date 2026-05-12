3876a4a50d6bf216d9dc4be8c1134973c72df200
xetra11 <xetra11@outlook.com>
2026-05-12 21:12:06 +0200

    feat(gadget): replace `Gadget` class with `Gadget` interface and implement `StoneGadget` for modular projectiles

--- Code/Gadget/Gadget.cs
+++ Code/Gadget/Gadget.cs
@@ -2,19 +2,7 @@ using System;
 using Sandbox;
 using Sandbox.Gadget;
 
-public sealed class Gadget : Component
+public interface Gadget
 {
-	[Property] public String Name { get; set; }
-	[Property] public GameObject ProjectilePrefab { get; set; }
-
-	public void UseGadget( Vector3 playerLocation, Vector3 playerFacingDirection )
-	{
-		var projectileInstance = ProjectilePrefab.Clone( playerLocation );
-		projectileInstance.GetComponent<Projectile>().Direction = playerFacingDirection;
-		Log.Info( $"Spawned projectile at {playerLocation}" );
-	}
-
-	protected override void OnUpdate()
-	{
-	}
+  public void UseGadget( Vector3 playerLocation, Vector3 playerFacingDirection );
 }
---
+++ Code/Gadget/StoneGadget.cs
@@ -0,0 +1,20 @@
+using System;
+using Sandbox;
+using Sandbox.Gadget;
+
+public sealed class StoneGadget : Component, Gadget
+{
+	[Property] public String Name { get; set; }
+	[Property] public GameObject ProjectilePrefab { get; set; }
+
+	public void UseGadget( Vector3 playerLocation, Vector3 playerFacingDirection )
+	{
+		var projectileInstance = ProjectilePrefab.Clone( playerLocation );
+		projectileInstance.GetComponent<Projectile>().Direction = playerFacingDirection;
+		Log.Info( $"Spawned projectile at {playerLocation}" );
+	}
+
+	protected override void OnUpdate()
+	{
+	}
+}
