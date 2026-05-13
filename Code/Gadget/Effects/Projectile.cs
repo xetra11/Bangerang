8c57599cb419e8a4df4a6ffc0d8b9e70928f8b83
xetra11 <xetra11@outlook.com>
2026-05-13 10:11:11 +0200

    feat(projectile): add velocity-based rotation and update prefab structure for modularity

--- Code/Gadget/Effects/Projectile.cs
+++ Code/Gadget/Effects/Projectile.cs
@@ -2,28 +2,34 @@ namespace Sandbox.Gadget;
 
 public class Projectile : Component
 {
-	[Property] public float ThrowSpeed { get; set; } = 1000f;
-	[Property] public float Gravity { get; set; } = 800f;
-	private Vector3 _direction;
-	private Vector3 _velocity;
+  [Property] public float ThrowSpeed { get; set; } = 1000f;
+  [Property] public float Gravity { get; set; } = 800f;
+  private Vector3 _direction;
+  private Vector3 _velocity;
 
-	public Vector3 Direction
-	{
-		set
-		{
-			_direction = value;
-		}
-	}
+  public Vector3 Direction
+  {
+    set
+    {
+      _direction = value;
+    }
+  }
 
-	protected override void OnStart()
-	{
-		_velocity = _direction * ThrowSpeed;
-	}
+  protected override void OnStart()
+  {
+    _velocity = _direction * ThrowSpeed;
+  }
 
-	protected override void OnUpdate()
-	{
-		if ( WorldPosition.z < -10 ) DestroyGameObject();
-		_velocity += Vector3.Down * Gravity * Time.Delta;
-		WorldPosition += _velocity * Time.Delta;
-	}
+  protected override void OnUpdate()
+  {
+    if ( WorldPosition.z < -10 ) DestroyGameObject();
+
+    _velocity += Vector3.Down * Gravity * Time.Delta;
+    WorldPosition += _velocity * Time.Delta;
+
+    if ( _velocity.Length > 0.001f )
+    {
+      WorldRotation = Rotation.LookAt( _velocity.Normal );
+    }
+  }
 }
