6d659884e4a8bdfec8e7cbb3aef24c3d1dd02325
xetra11 <xetra11@outlook.com>
2026-06-04 20:32:28 +0200

    git(init): init)

---
+++ Source/Game/Test.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using FlaxEngine;
+
+namespace Game;
+
+/// <summary>
+/// Test Script.
+/// </summary>
+public class Test : Script
+{
+    public AnimatedModel AnimatedModel;
+
+    /// <inheritdoc/>
+    public override void OnStart()
+    {
+        // Here you can add code that needs to be called when script is created, just before the first game update
+    }
+
+    /// <inheritdoc/>
+    public override void OnEnable()
+    {
+        // Here you can add code that needs to be called when script is enabled (eg. register for events)
+    }
+
+    /// <inheritdoc/>
+    public override void OnDisable()
+    {
+        // Here you can add code that needs to be called when script is disabled (eg. unregister from events)
+    }
+
+    /// <inheritdoc/>
+    public override void OnUpdate()
+    {
+        if (Input.GetKey(KeyboardKeys.W))
+        {
+             AnimatedModel.SetParameterValue("IsWalking", true);
+             return;
+        }
+        AnimatedModel.SetParameterValue("IsWalking", false);
+    }
+}
